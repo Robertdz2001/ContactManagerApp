6bd9d4ae0490fe5b374ffe41d24ba6ef69db5227
Robertdz2001 <robertdz0@onet.pl>
2022-09-27 10:54:53 +0200

    Add functionality to delete button

--- contactmanager-front/src/App/Contact/Contact.js
+++ contactmanager-front/src/App/Contact/Contact.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./Contact.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import { EyeFill, PenFill, TrashFill } from 'react-bootstrap-icons';
+import { Navigate } from "react-router-dom";
 class Contact extends React.Component {
     constructor(props) {
         super(props);
+        this.delete = this.delete.bind(this);
+    }
+
+    async delete(e) {
+        const url = "https://localhost:7173/contacts/";
+        const idParam = this.props.contact.id;
+        const urlFetch = url + idParam;
+        await fetch(urlFetch, { method: 'DELETE' });
+        window.location.reload(false);
+
     }
 
     render() {
         return (
             <div className="contact">
-                <div className="contactImg"><img src={`${this.props.contact.photoUrl}`} height="180px" width="250px" /></div>
+                <div className="contactImg"><img src={`${this.props.contact.photoUrl}`} height="180px" width="180px" /></div>
                 <div className="contactInfo">
                     <table>
                         <tbody>
@@ -21,9 +32,9 @@ class Contact extends React.Component {
                     </table>
                 </div>
                 <div className="contactButtons">
-                    <div className="Button"><button type="button" class="btn btn-warning btn-lg"><EyeFill /></button></div>
-                    <div className="Button"><button type="button" class="btn btn-primary btn-lg"><PenFill /></button></div>
-                    <div className="Button"><button type="button" class="btn btn-danger btn-lg"><TrashFill /></button></div>
+                    <div className="Button"><button type="button" className="btn btn-warning btn-lg"><EyeFill /></button></div>
+                    <div className="Button"><button type="button" className="btn btn-primary btn-lg"><PenFill /></button></div>
+                    <div className="Button"><button type="button" className="btn btn-danger btn-lg" onClick={this.delete}><TrashFill /></button></div>
                 </div>
             </div>
         );
